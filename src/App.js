0b94aec5f58fa2848a41833d17f861b02e2adc06
mikrob <promikrob@gmail.com>
2022-12-15 19:45:34 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  function test() {
+    setCount(count + 1);
+  }
+
   return (
     <>
-      <div></div>
-      <div></div>
+      <div onClick={() => test}></div>
+      <div>{count}</div>
     </>
   );
 }
